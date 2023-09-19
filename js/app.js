ab88c6680a07e54ff6d567b41f3075e60b4e4f73
alihaghayegh <haghayeghali95@gmail.com>
2023-09-19 10:14:29 +0330

    selectors and event targets added to javascript and it works

--- js/app.js
+++ js/app.js
@@ -5,13 +5,28 @@ const App = {
     menuItem: document.querySelector("[data-id='menu-items']"),
     resetBtn: document.querySelector("[data-id='reset-btn']"),
     newRoundBtn: document.querySelector("[data-id='new-round-btn']"),
+    squares: document.querySelectorAll("[data-id=square]"),
   },
 
   init() {
     App.$.menu.addEventListener("click", (event) => {
       App.$.menuItem.classList.toggle("hidden");
     });
+
+    App.$.resetBtn.addEventListener("click", (event) => {
+      console.log("Reset");
+    });
+
+    App.$.newRoundBtn.addEventListener("click", (event) => {
+      console.log("New Round");
+    });
+
+    App.$.squares.forEach((square) => {
+      square.addEventListener("click", (event) => {
+        console.log(`Square with id ${event.target.id}`);
+      });
+    });
   },
 };
 
-window.addEventListener('load', App.init)
+window.addEventListener("load", App.init);
