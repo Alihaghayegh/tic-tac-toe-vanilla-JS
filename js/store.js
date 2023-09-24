3aa153baf8b90ca5ee97fe23f099d658c8299bc1
alihaghayegh <haghayeghali95@gmail.com>
2023-09-24 11:48:40 +0330

    state management for the game added in store.js

---
+++ js/store.js
@@ -0,0 +1,32 @@
+const initalValue = {
+  moves: [],
+};
+
+export default class Store {
+  #state = initalValue;
+
+  constructor() {}
+
+  #getState() {
+    return this.#state;
+  }
+
+  #saveState(stateOrFn) {
+    const prevState = this.#getState();
+
+    let newState;
+
+    switch (typeof stateOrFn) {
+      case "function":
+        newState = stateOrFn(prevState);
+        break;
+      case "object":
+        newState = stateOrFn;
+        break;
+      default:
+        throw new Error("Invalid argument passed to saveState");
+    }
+
+    this.#state = newState;
+  }
+}
